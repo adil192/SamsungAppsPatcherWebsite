b5e978ed3246129d7867dfff5a670970932097db
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-03-14 19:43:53 +0000

    SamsungAppsPatcher: add dropdowns to download buttons

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 /** @noinspection PhpIllegalPsrClassPathInspection */
-
 include_once "../global_tools.php";
 ?>
 <!doctype html>
@@ -36,13 +35,13 @@ include_once "../global_tools.php";
 <?php
 class SamsungApp {
     public string $displayName;
-    public string $dl;
+    public array $dls;
     public string $icon;
     public string $date;
 
-    public function __construct(string $displayName, string $dl = null, string $icon = null, $date = null) {
+    public function __construct(string $displayName, string $icon = null, $date = null, array $dls = null) {
         $this->displayName = $displayName;
-        $this->dl = $dl ?? "";
+        $this->dls = $dls ?? [];
         $this->icon = $icon ?? "fallback.webp";
         $this->date = $date ?? "";
     }
@@ -57,8 +56,25 @@ function genApps(array $samsungApps) {
             <div class="card-body">
                 <h3 class="card-title"><?=$app->displayName?></h3>
                 <p class="card-text"><?=$app->date?></p>
-                <?php if (!empty($app->dl)) { ?>
-                    <a href="dl/<?=$app->dl?>" class="btn btn-primary">Download</a>
+                <?php
+                $count = count($app->dls);
+                if ($count > 0) {
+                    ?>
+                    <div class="btn-group dropdown">
+                        <a class="btn btn-primary" href="dl/<?=$app->dls[0][1]?>">
+                            Download
+                        </a>
+                        <button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                            <span class="visually-hidden">View alternate downloads</span>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="<?=$app->dls[0][1]?>"><?=$app->dls[0][0]?></a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <?php for ($d = 1; $d < $count; ++$d) { ?>
+                                <li><a class="dropdown-item" href="<?=$app->dls[$d][1]?>"><?=$app->dls[$d][0]?></a></li>
+                            <?php } ?>
+                        </ul>
+                    </div>
                 <?php } ?>
             </div>
         </div>
@@ -71,9 +87,16 @@ function genApps(array $samsungApps) {
 <div class="container">
     <h2>Core Apps</h2>
 	<?php genApps([
-		new SamsungApp("Samsung Health", "shealth.apk", "SamsungHealth.webp", "14 February 2022"),
-		new SamsungApp("Samsung Wearable", "wearable.apk", "Wear.webp", "8 January 2022"),
-		new SamsungApp("Accessory Service", "accessoryservice.apk", "SamsungAccessoryService.webp", "29 December 2021"),
+		new SamsungApp("Samsung Health", "SamsungHealth.webp", "10 March 2022", [
+			["6.21.1.031", "shealth.6.21.1.031.apk"],
+			["oldest", "shealth.apk"]
+		]),
+		new SamsungApp("Samsung Wearable", "Wear.webp", "8 January 2022", [
+			["oldest", "wearable.apk"]
+		]),
+		new SamsungApp("Accessory Service", "SamsungAccessoryService.webp", "29 December 2021", [
+			["oldest", "accessoryservice.apk"]
+		]),
 	]); ?>
 </div>
 
@@ -82,13 +105,27 @@ function genApps(array $samsungApps) {
 <div class="container">
     <h2>Watch plugins</h2>
 	<?php genApps([
-		new SamsungApp("Galaxy Watch Plugin", "watchplugin.apk", "Wear.webp", "14 February 2022"),
-		new SamsungApp("Galaxy Watch3 Plugin", "watch3plugin.apk", "Wear.webp", "14 February 2022"),
-		new SamsungApp("Galaxy Watch4 Plugin", "watch4plugin.apk", "Wear.webp", "9 January 2022"),
-		new SamsungApp("Watch Active Plugin", "activeplugin.apk", "Wear.webp", "14 February 2022"),
-		new SamsungApp("Watch Active2 Plugin", "active2plugin.apk", "Wear.webp", "14 February 2022"),
-		new SamsungApp("Gear Fit2 Plugin", "gearfit2plugin.apk", "Wear.webp", "7 January 2022"),
-		new SamsungApp("Gear S Plugin", "gearsportplugin.apk", "Wear.webp", "14 February 2022"),
+		new SamsungApp("Galaxy Watch Plugin", "Wear.webp", "14 February 2022", [
+			["oldest", "watchplugin.apk"]
+		]),
+		new SamsungApp("Galaxy Watch3 Plugin", "Wear.webp", "14 February 2022", [
+			["oldest", "watch3plugin.apk"]
+		]),
+		new SamsungApp("Galaxy Watch4 Plugin", "Wear.webp", "9 January 2022", [
+			["oldest", "watch4plugin.apk"]
+		]),
+		new SamsungApp("Watch Active Plugin", "Wear.webp", "14 February 2022", [
+			["oldest", "activeplugin.apk"]
+		]),
+		new SamsungApp("Watch Active2 Plugin", "Wear.webp", "14 February 2022", [
+			["oldest", "active2plugin.apk"]
+		]),
+		new SamsungApp("Gear Fit2 Plugin", "Wear.webp", "7 January 2022", [
+			["oldest", "gearfit2plugin.apk"]
+		]),
+		new SamsungApp("Gear S Plugin", "Wear.webp", "14 February 2022", [
+			["oldest", "gearsportplugin.apk"]
+		]),
 	]); ?>
 </div>
 
@@ -97,11 +134,21 @@ function genApps(array $samsungApps) {
 <div class="container">
     <h2>Other plugins</h2>
 	<?php genApps([
-		new SamsungApp("Galaxy Buds Manager", "budsplugin.apk", null, "16 January 2022"),
-		new SamsungApp("Galaxy Buds Pro Manage", "budsproplugin.apk", "Buds.webp", "30 January 2022"),
-		new SamsungApp("Galaxy Buds2 Manager", "buds2plugin.apk", "Buds.webp", "30 January 2022"),
-		new SamsungApp("Galaxy Buds+ Manager", "budsplusplugin.apk", "Buds.webp", "16 January 2022"),
-		new SamsungApp("Galaxy Buds Live Manager", "budsliveplugin.apk", "Buds.webp", "16 January 2022"),
+		new SamsungApp("Galaxy Buds Manager", null, "16 January 2022", [
+			["oldest", "budsplugin.apk"]
+		]),
+		new SamsungApp("Galaxy Buds Pro Manager", "Buds.webp", "30 January 2022", [
+			["oldest", "budsproplugin.apk"]
+		]),
+		new SamsungApp("Galaxy Buds2 Manager", "Buds.webp", "30 January 2022", [
+			["oldest", "buds2plugin.apk"]
+		]),
+		new SamsungApp("Galaxy Buds+ Manager", "Buds.webp", "16 January 2022", [
+			["oldest", "budsplusplugin.apk"]
+		]),
+		new SamsungApp("Galaxy Buds Live Manager", "Buds.webp", "16 January 2022", [
+			["oldest", "budsliveplugin.apk"]
+		]),
 	]); ?>
 </div>
 
@@ -113,6 +160,7 @@ function genApps(array $samsungApps) {
         in <a href="https://mega.nz/folder/sUFj2C5b#M4zEP-c9ylY-ENxPw7qCUQ">this MEGA drive</a>.</p>
 </div>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </body>
 </html>
