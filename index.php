f82164d31120156c33265b7f57325e88c184c549
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-03-14 12:16:20 +0000

    SamsungAppsPatcher: move inline styles to css file

--- index.php
+++ index.php
@@ -17,29 +17,7 @@ include_once "../global_tools.php";
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <style>
-        .header {
-            padding-top: 1rem;
-        }
-        .header-row {
-            display: flex;
-            flex-flow: row nowrap;
-            gap: 1rem;
-        }
-        .github-icon {
-            width: calc(1.375rem + 1.5vw);
-            height: 100%;
-            object-fit: contain;
-        }
-        .card {
-            width: 10rem;
-        }
-        .cards-parent {
-            display: flex;
-            flex-flow: row wrap;
-            gap: 1rem;
-        }
-    </style>
+    <link href="SamsungAppsPatcher.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 
